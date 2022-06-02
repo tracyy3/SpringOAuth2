c435aeed33cb065c501e58b6e4f79669e8bedd4f
tracy <tracyyang336@gmail.com>
2022-06-02 12:46:03 -0400

    first commit

---
+++ socialapplication/src/test/java/com/socialapplication/socialapplication/SocialapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialapplication.socialapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
